581f4794d2c31932447fc4e0cd00998fa6a2b2cb
AlanHSS <alanhsoaress@gmail.com>
2025-09-23 15:28:50 -0300

    first commit

---
+++ src/main/java/com/alanhss/ClashZone/ClashZoneApplication.java
@@ -0,0 +1,13 @@
+package com.alanhss.ClashZone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClashZoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClashZoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alanhss/ClashZone/ClashZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alanhss.ClashZone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClashZoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
