e89f7151f1bfac4c41c3ac0c9b42d90b19c2c33f
AlanHSS <alanhsoaress@gmail.com>
2025-09-24 17:57:31 -0300

    feat(cora): cria a entidade tornio

--- src/main/java/com/alanhss/ClashZone/core/entities/Torneio.java
+++ src/main/java/com/alanhss/ClashZone/core/entities/Torneio.java
@@ -1,16 +1,19 @@
 package com.alanhss.ClashZone.core.entities;
 
 import com.alanhss.ClashZone.core.enums.Games;
+import com.alanhss.ClashZone.core.enums.Plataforma;
+import com.alanhss.ClashZone.core.enums.StatusDoTorneio;
 
 import java.time.LocalDateTime;
 
 
-public record Torneio(Integer id,
+public record Torneio(Long id,
                       String nomeDoTorneio,
                       String descricaoDoTorneio,
                       LocalDateTime inicioDoTorneio,
                       Games jogoDoTorneio,
                       Integer quantidadeDeEquipes,
-                      Integer quantidadeDePessoasPorEquipe,
-                      String criadorDoTorneio
+                      String criadorDoTorneio,
+                      StatusDoTorneio statusDoTorneio,
+                      Plataforma plataforma
 ) {}
