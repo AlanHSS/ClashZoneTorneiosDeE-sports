de670483b8fd17ef5f086c86191ccc7a95f8514f
AlanHSS <alanhsoaress@gmail.com>
2025-09-24 17:52:13 -0300

    build: cria a primeira versão da tabela de torneio

---
+++ src/main/resources/db/migration/V1__criar_tabela_torneio.sql
@@ -0,0 +1,12 @@
+-- V1_criar_tabela_torneio.sql
+CREATE TABLE torneio (
+    id BIGSERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    descricao TEXT,
+    inicio TIMESTAMP NOT NULL,
+    jogo VARCHAR(50) NOT NULL,
+    quantidade_equipes INTEGER NOT NULL,
+    criador VARCHAR(100) NOT NULL,
+    status VARCHAR(20) NOT NULL DEFAULT 'AGENDADO',
+    plataforma VARCHAR(50) NOT NULL
+);
