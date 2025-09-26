ee029fdc110512cf707d31c51eb8098d5867ee4a
AlanHSS <alanhsoaress@gmail.com>
2025-09-26 16:09:08 -0300

    feat: cria o repository e controller do torneio

---
+++ src/main/java/com/alanhss/ClashZone/infra/persistence/TorneioRepository.java
@@ -0,0 +1,6 @@
+package com.alanhss.ClashZone.infra.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TorneioRepository extends JpaRepository<TorneioEntity, Long> {
+}
