3b5945072527ae72c33ed62691643554bed935d2
AlanHSS <alanhsoaress@gmail.com>
2025-09-30 17:11:55 -0300

    feat: cria o mapper entity-domain

---
+++ src/main/java/com/alanhss/ClashZone/infra/mappers/TorneioEntityMapper.java
@@ -0,0 +1,38 @@
+package com.alanhss.ClashZone.infra.mappers;
+
+import com.alanhss.ClashZone.core.entities.TorneioDomain;
+import com.alanhss.ClashZone.infra.persistence.TorneioEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TorneioEntityMapper {
+
+    public TorneioEntity toEntity(TorneioDomain torneioDomain){
+        return new TorneioEntity(
+                torneioDomain.id(),
+                torneioDomain.nomeDoTorneio(),
+                torneioDomain.descricaoDoTorneio(),
+                torneioDomain.inicioDoTorneio(),
+                torneioDomain.jogoDoTorneio(),
+                torneioDomain.quantidadeDeEquipes(),
+                torneioDomain.criadorDoTorneio(),
+                torneioDomain.statusDoTorneio(),
+                torneioDomain.plataforma()
+        );
+    }
+
+    public TorneioDomain toDomain(TorneioEntity torneioEntity){
+        return new TorneioDomain(
+                torneioEntity.getId(),
+                torneioEntity.getNomeDoTorneio(),
+                torneioEntity.getDescricaoDoTorneio(),
+                torneioEntity.getInicioDoTorneio(),
+                torneioEntity.getJogoDoTorneio(),
+                torneioEntity.getQuantidadeDeEquipes(),
+                torneioEntity.getCriadorDoTorneio(),
+                torneioEntity.getStatusDoTorneio(),
+                torneioEntity.getPlataforma()
+        );
+    }
+
+}
