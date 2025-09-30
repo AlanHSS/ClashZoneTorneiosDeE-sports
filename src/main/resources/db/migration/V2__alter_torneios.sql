37dc18f32d595f16dab2247f295efe00f0e00471
AlanHSS <alanhsoaress@gmail.com>
2025-09-30 18:14:27 -0300

    feat: cria a V2 do banco de dados, alterando os nomes das colunas

---
+++ src/main/resources/db/migration/V2__alter_torneios.sql
@@ -0,0 +1,7 @@
+ALTER TABLE torneios RENAME COLUMN nome TO nome_do_torneio;
+ALTER TABLE torneios RENAME COLUMN descricao TO descricao_do_torneio;
+ALTER TABLE torneios RENAME COLUMN inicio TO inicio_do_torneio;
+ALTER TABLE torneios RENAME COLUMN jogo TO jogo_do_torneio;
+ALTER TABLE torneios RENAME COLUMN quantidade_equipes TO quantidade_de_equipes;
+ALTER TABLE torneios RENAME COLUMN criador TO criador_do_torneio;
+ALTER TABLE torneios RENAME COLUMN status TO status_do_torneio;
