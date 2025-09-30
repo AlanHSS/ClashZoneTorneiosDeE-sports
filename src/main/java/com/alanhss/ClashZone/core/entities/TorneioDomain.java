b5db0774db1cfb811a348350258e281f604db040
AlanHSS <alanhsoaress@gmail.com>
2025-09-30 16:30:35 -0300

    refactor: altera o nome da classe de Torneio para TorneioDomain

---
+++ src/main/java/com/alanhss/ClashZone/core/entities/TorneioDomain.java
@@ -0,0 +1,19 @@
+package com.alanhss.ClashZone.core.entities;
+
+import com.alanhss.ClashZone.core.enums.Games;
+import com.alanhss.ClashZone.core.enums.Plataforma;
+import com.alanhss.ClashZone.core.enums.StatusDoTorneio;
+
+import java.time.LocalDateTime;
+
+
+public record TorneioDomain(Long id,
+                            String nomeDoTorneio,
+                            String descricaoDoTorneio,
+                            LocalDateTime inicioDoTorneio,
+                            Games jogoDoTorneio,
+                            Integer quantidadeDeEquipes,
+                            String criadorDoTorneio,
+                            StatusDoTorneio statusDoTorneio,
+                            Plataforma plataforma
+) {}
