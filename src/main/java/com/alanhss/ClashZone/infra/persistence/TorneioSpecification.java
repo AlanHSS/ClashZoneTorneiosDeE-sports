323948e2e8d013cdec4c6b48f7a18c3f032435c4
AlanHSS <alanhsoaress@gmail.com>
2025-10-08 17:04:15 -0300

    feat: cria o Specification para fazer a filtragem de torneios

---
+++ src/main/java/com/alanhss/ClashZone/infra/persistence/TorneioSpecification.java
@@ -0,0 +1,45 @@
+package com.alanhss.ClashZone.infra.persistence;
+
+import com.alanhss.ClashZone.core.enums.Games;
+import com.alanhss.ClashZone.core.enums.Plataforma;
+import com.alanhss.ClashZone.core.enums.StatusDoTorneio;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+public class TorneioSpecification {
+
+    private final TorneioRepository torneioRepository;
+
+    public static Specification<TorneioEntity> comNome(String nome) {
+        return (root, query, cb) ->
+                nome == null ? null :
+                        cb.like(cb.lower(root.get("nomeDoTorneio")), "%" + nome.toLowerCase() + "%");
+    }
+
+    public static Specification<TorneioEntity> comJogo(Games jogo) {
+        return (root, query, cb) ->
+                jogo == null ? null :
+                        cb.equal(root.get("jogoDoTorneio"), Games.valueOf(String.valueOf(jogo)));
+    }
+
+    public static Specification<TorneioEntity> comPlataforma(Plataforma plataforma) {
+        return (root, query, cb) ->
+                plataforma == null ? null :
+                        cb.equal(root.get("plataforma"), Plataforma.valueOf(String.valueOf(plataforma)));
+    }
+
+    public static Specification<TorneioEntity> comStatus(StatusDoTorneio status) {
+        return (root, query, cb) ->
+                status == null ? null :
+                        cb.equal(root.get("statusDoTorneio"), StatusDoTorneio.valueOf(String.valueOf(status)));
+    }
+
+    public static Specification<TorneioEntity> comDataInicio(LocalDateTime dataInicio) {
+        return (root, query, cb) ->
+                dataInicio == null ? null :
+                        cb.greaterThanOrEqualTo(root.get("inicioDoTorneio"), dataInicio);
+    }
+}
