97c33c6708078388a81fa12d68ab014a444531a7
AlanHSS <alanhsoaress@gmail.com>
2025-10-18 15:26:32 -0300

    feat: cria o usecase para criar um novo usuário

---
+++ src/main/java/com/alanhss/ClashZone/core/usecases/CriarUsuarioUsecase.java
@@ -0,0 +1,9 @@
+package com.alanhss.ClashZone.core.usecases;
+
+import com.alanhss.ClashZone.core.entities.UsuariosDomain;
+
+public interface CriarUsuarioUsecase {
+
+    public UsuariosDomain execute(UsuariosDomain usuariosDomain);
+
+}
---
+++ src/main/java/com/alanhss/ClashZone/core/usecases/CriarUsuarioUsecaseImpl.java
@@ -0,0 +1,18 @@
+package com.alanhss.ClashZone.core.usecases;
+import com.alanhss.ClashZone.core.entities.UsuariosDomain;
+import com.alanhss.ClashZone.core.gateway.UsuariosGateway;
+
+public class CriarUsuarioUsecaseImpl implements CriarUsuarioUsecase{
+
+    private final UsuariosGateway usuariosGateway;
+
+    public CriarUsuarioUsecaseImpl(UsuariosGateway usuariosGateway) {
+        this.usuariosGateway = usuariosGateway;
+    }
+
+    @Override
+    public UsuariosDomain execute(UsuariosDomain usuariosDomain) {
+        return usuariosGateway.criarUsuario(usuariosDomain);
+    }
+
+}
