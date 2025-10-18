66692b452ad9ea2db99682b671a82fcc636ad5c6
AlanHSS <alanhsoaress@gmail.com>
2025-10-18 15:28:49 -0300

    feat: cria a entidade Usuários

---
+++ src/main/java/com/alanhss/ClashZone/infra/persistence/UsuariosEntity.java
@@ -0,0 +1,25 @@
+package com.alanhss.ClashZone.infra.persistence;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "usuarios")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UsuariosEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nomeDoUsuario;
+
+    private String emailDoUsuario;
+
+    private String senhaDoUsuario;
+
+}
