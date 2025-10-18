dad1863b37f6c60a54aece644078a278b96e4114
AlanHSS <alanhsoaress@gmail.com>
2025-10-18 15:28:26 -0300

    feat: cria o UsuariosRepository

---
+++ src/main/java/com/alanhss/ClashZone/infra/persistence/UsuariosRepository.java
@@ -0,0 +1,6 @@
+package com.alanhss.ClashZone.infra.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuariosRepository extends JpaRepository<UsuariosEntity, Long> {
+}
