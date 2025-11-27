2b475b20c09f3f780cc5e1c88447ff8f105a557c
AlanHSS <alanhsoaress@gmail.com>
2025-11-27 16:33:55 -0300

    refactor: adiciona informações de cada como nome de exibição, jogadores titulares, reservas e permite reservas

--- src/main/java/com/alanhss/ClashZone/core/enums/Games.java
+++ src/main/java/com/alanhss/ClashZone/core/enums/Games.java
@@ -1,16 +1,52 @@
 package com.alanhss.ClashZone.core.enums;
 
 public enum Games {
-    LEAGUE_OF_LEGENDS,
-    CS2,
-    DOTA2,
-    VALORANT,
-    OVERWATCH_2,
-    RAINBOW_SIX_SIEGE,
-    FIFA,
-    STREET_FIGHTERS_6,
-    TEKKEN_8,
-    FORTNITE,
-    PUBG,
-    OUTRO;
+    LEAGUE_OF_LEGENDS("League of Legends", 5, 2, true),
+    CS2("Counter-Strike 2", 5, 2, true),
+    DOTA2("Dota 2", 5, 2, true),
+    VALORANT("Valorant", 5, 2, true),
+    OVERWATCH_2("Overwatch 2", 5, 3, true),
+    RAINBOW_SIX_SIEGE("Rainbow Six Siege", 5, 2, true),
+    FIFA("FIFA", 1, 0, false),
+    STREET_FIGHTERS_6("Street Fighter 6", 1, 0, false),
+    TEKKEN_8("Tekken 8", 1, 0, false),
+    FORTNITE("Fortnite", 4, 1, true),
+    PUBG("PUBG", 4, 1, true),
+    OUTRO("Outro", 1, 0, false);
+
+    private final String nomeExibicao;
+    private final int jogadoresTitulares;
+    private final int jogadoresReservas;
+    private final boolean permiteReservas;
+
+    Games(String nomeExibicao, int jogadoresTitulares, int jogadoresReservas, boolean permiteReservas) {
+        this.nomeExibicao = nomeExibicao;
+        this.jogadoresTitulares = jogadoresTitulares;
+        this.jogadoresReservas = jogadoresReservas;
+        this.permiteReservas = permiteReservas;
+    }
+
+    public String getNomeExibicao() {
+        return nomeExibicao;
+    }
+
+    public int getJogadoresTitulares() {
+        return jogadoresTitulares;
+    }
+
+    public int getJogadoresReservas() {
+        return jogadoresReservas;
+    }
+
+    public boolean isPermiteReservas() {
+        return permiteReservas;
+    }
+
+    public int getTotalMaximoJogadores() {
+        return jogadoresTitulares + jogadoresReservas;
+    }
+
+    public int getMinimoJogadores() {
+        return jogadoresTitulares;
+    }
 }
