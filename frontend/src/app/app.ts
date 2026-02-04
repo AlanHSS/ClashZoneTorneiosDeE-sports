a7ab28d9f834446351d64ed8bcf81b4794258d01
AlanHSS <alanhsoaress@gmail.com>
2026-02-04 16:46:53 -0300

    feat: cria as pastas e subpastas da estrutura do frontend

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -3,11 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { Home } from './components/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbarModule, MatIconModule, MatButtonModule, Home],
+  imports: [RouterOutlet, MatToolbarModule, MatIconModule, MatButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
