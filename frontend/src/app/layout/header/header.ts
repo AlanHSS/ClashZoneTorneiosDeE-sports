b031b732d8a4e994957379fe091c47a8e1ffec3c
AlanHSS <alanhsoaress@gmail.com>
2026-02-11 15:49:39 -0300

    feat: cria os layout header e footer

---
+++ frontend/src/app/layout/header/header.ts
@@ -0,0 +1,30 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { AuthService } from '@core/services/auth.service';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  authService = inject(AuthService);
+
+  onLogout(): void {
+    this.authService.logout();
+  }
+}
